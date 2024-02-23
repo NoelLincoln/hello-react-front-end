be5d25015b49dd6f8e491ad346910d0e4fb6ec9c
Noel Bryant <noelsobryant@gmail.com>
2024-02-23 06:32:53 +0300

    fetch random greeting from rails API

---
+++ src/components/Greeting.js
@@ -0,0 +1,52 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import store from '../redux/store'; // Import your Redux store
+
+import { fetchRandomGreeting } from '../redux/greetings/greetingsSlice';
+
+function Greeting() {
+  const dispatch = useDispatch();
+  const { greeting, isLoading, error } = useSelector(
+    (state) => state.greetings,
+  );
+  console.log(greeting);
+
+  console.log(store.getState());
+
+  useEffect(() => {
+    dispatch(fetchRandomGreeting());
+  }, [dispatch]);
+
+  const handleClick = () => {
+    dispatch(fetchRandomGreeting());
+  };
+
+  if (isLoading) {
+    return (
+      <>
+        <div className="loading">...isLoading</div>
+      </>
+    );
+  }
+
+  if (error) {
+    return (
+      <>
+        <div className="loading">
+          Something went wrong while fetching the data
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className="greeting">{greeting.message}</div>
+      <button type="button" onClick={handleClick}>
+        Get new greeting
+      </button>
+    </>
+  );
+}
+
+export default Greeting;
